cae49e51feb8bd10f665b1197786c1baace513f7
jrich523 <jrich523@gmail.com>
2014-04-04 13:52:00 -0700

    should work!?

--- GitHub.psm1
+++ GitHub.psm1
@@ -18,43 +18,188 @@ function Publish-Git
         [Parameter(Mandatory=$true,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        $Path = '.'
-
+        $Path = '.',
+        $Certificate,
+        $title,
+        $Description,
+        $Remote="origin",
+        [switch]$draft,
+        [switch]$Prerelease,
+        [switch]$IncrementVersion
         
 
 
     )
     ## make sure its committed and all is ok
-        
-    $branch = git branch | ? {$_ -match "^\*\s+(.+)"} | %{$matches[1]}
-    Write-Verbose "Current Branch: $branch"
+    $path = gi $path
+    Push-Location
+    Set-Location $Path
     
-    $status = git status $path --short|?{$_ -match '(.)(.)\s+(.+)'} | %{[pscustomobject]@{X=$matches[1];Y=$matches[2];File=$matches[3]}}
+    #not safe!
+    #populate what info we can from the path
+    $username, $repo = ((git remote -v | ?{$_ -like "*(push)"}).replace('.git','') -split '\s+')[1].split("/") | select -last 2
+    $branch = git branch | ? {$_ -match "^\*\s+(.+)"} | %{$matches[1]}
+
+    Write-Verbose "Username: $username"
+    Write-Verbose "Repository: $repo"
+    Write-Verbose "Branch: $branch"
+    
+    $status = git status --short|?{$_ -match '(.)(.)\s+(.+)'} | %{[pscustomobject]@{X=$matches[1];Y=$matches[2];File=$matches[3]}}
     if($status)
     {
-        Write-Error "Work not commited!"
-        break
+        if($status | ?{$_.x -ne '?'})
+        {
+            Write-Error "Work not commited!"
+            break
+        }
     }
     Write-Verbose "Status: Committed and up to date"
     
     ## update version and commit?
+    ## this will be replaced, just a test for commit purposes
+    $psd1 = gi ((gi $path).name + ".psd1")
+    (gc $psd1) | %{
+    if($_ -match 'moduleversion\s*=(.+)')
+    {
+        $version = [version]($Matches[1].trim() -replace '''|"',"")
+        Write-Verbose "Current Version: $version"
+        if($IncrementVersion){$minor = $version.Minor+1}else{$version.Minor}
+        $versionshort = "$($version.Major).$($minor)"
+        $versionfull = "$versionshort.$(Get-Date -Format "yyyyMMdd.HHmm")"
+        "ModuleVersion='$versionfull'"
+        Write-Verbose "New Versioin: $versionfull"
+ 
+    }
+    else{$_}
+    } | Out-File $psd1
 
-    ## sign files
-    ## add to package (zip/nuget)
-    ## sign packages?
-    ## roll back changes
-    ## add tag
-    ## create release
+    #commit version change
+    git commit -a -m "Release Commit - Version Tag $versionfull" | Out-Null
+    git push origin $branch
+    $tag = "V$versionshort"
+    git tag -a $tag -m "Auto Release version $versionfull"
+    git push --tags
+
+    $token = Get-GitToken -Credential $Credential
+    
+    $GHRelease = New-GitHubRelease -username $username -repo $repo -token $token -tag $tag -branch $branch -Title $title -Description $Description -draft $draft -Prerelease $Prerelease
+    
+    ## create a release folder, maybe clear it first?
+    
+    $releaseFolder = join-path $path Release
+    mkdir $releaseFolder -ea 0
+       
+    #create a folder for module files
+    $modtemp = join-path $releaseFolder (Split-Path $path -Leaf)
+    mkdir $modtemp -ea 0
+    #get all files
+    $filenames = git ls-tree -r $branch --name-only | ? {$_ -notlike ".*"}
+    $files = $filenames | %{join-path . $_} | gi
+    #copy files to module temp/release location
+    copy $files $modtemp
+        
+    if($Certificate)
+    {
+        gci $modtemp | Set-AuthenticodeSignature -Certificate $Certificate
+    }
+
+    
+    ## create packages
+    #poshcode/nuget/chocolatey
+    ipmo $modtemp
+    Write-Verbose "Creating nuget package"
+    $nugetfiles = Compress-Module -Module (split-path $modtemp -Leaf) -OutputPath $releaseFolder -Force
+    Write-Verbose "uploading nuget stuff"
+    $nugetfiles | %{Add-GitHubReleaseAsset -token $token -id $GHRelease.id -username $username -repo $repo -file $_}
+    ## zip
+    Write-Verbose "Creating zip package"
+    [Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+    $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+    $zipfile = Join-Path $releaseFolder "$($modtemp.BaseName).zip"
+    [System.IO.Compression.ZipFile]::CreateFromDirectory($modtemp,$zipfile,$compressionLevel,$true)
+    if($Certificate)
+    {
+        Write-Verbose "Signing zip"
+        Set-AuthenticodeSignature -FilePath $zipfile -Certificate $cert
+    }
+    Write-Verbose "Uploading zip"
+    Add-GitHubReleaseAsset -token $token -id $GHRelease.id -username $username -repo $repo -file $zipfile
+    
+    Pop-Location
     
 }
 
+function New-GitHubRelease
+{
+param(  $username,
+        $repo,
+        $token,
+        $tag,
+        $branch='master',
+        $Title,
+        $Description,
+        [switch]$draft,
+        [switch]$Prerelease
+        )
+    $url = "https://api.github.com/repos/$username/$repo/releases?access_token=$token"
 
-if($Increment) { 
-  if($Version.Revision -ge 0) { 
-    $Version = New-Object Version $Version.Major, $Version.Minor, $Version.Build, ($Version.Revision + 1) 
-  } elseif($Version.Build -ge 0) { 
-    $Version = New-Object Version $Version.Major, $Version.Minor, ($Version.Build + 1) 
-  } elseif($Version.Minor -ge 0) { 
-    $Version = New-Object Version $Version.Major, ($Version.Minor + 1) 
-  } 
+    $details = ConvertTo-Json @{
+          tag_name = $tag
+          target_commitish=$branch
+          name=$title
+          body= $Description
+          draft= $draft.IsPresent
+          prerelease= $Prerelease.IsPresent
+    }
+
+    Invoke-RestMethod -Uri $url -body $json -Method Post -ContentType 'application/json'
+}
+#part of $rtn
+Function Add-GitHubReleaseAsset
+{
+param($token,
+        $id,
+        $username,
+        $repo,
+        $file)
+    $uploadURL = "https://uploads.github.com/repos/$username/$repo/releases/$id/assets?name=$($file.name)&access_token=$token"
+    irm -Uri $uploadURL -ContentType 'application/zip' -Body ([IO.File]::ReadAllBytes($file)) -Method Post
+}
+
+function Get-GitToken
+{
+param($Credential)
+    $params = @{
+          Uri = 'https://api.github.com/authorizations';
+          Headers = @{
+            Authorization = 'Basic ' + [Convert]::ToBase64String(
+              [Text.Encoding]::ASCII.GetBytes("$($Credential.UserName):$($Credential.GetNetworkCredential().Password)"));
+          }
+
+        }
+    $rtn = Invoke-RestMethod @params
+
+    #is looking for repo scope good enough? think so
+
+    if($auth = $rtn | ? scopes -Contains "repo")
+    {
+        $auth.token
+    }
+    else #create token
+    {
+        $data = @{
+            scopes = @('repo')
+            Note = "Created by ModuleBuilder"
+            }
+        $params.Add("ContentType",'application/json')
+        $params.add("Body",(ConvertFrom-Json $data))
+        try
+        {
+            (irm @params).token
+        }
+        catch
+        {
+            Write-Error "An unexpected error occurred (bad user/password?) $($Error[0])"
+        }
+    }
 }
