18f0465ce55727ad4d80a1cbc795254615e2e1e8
Justin <jrich523@gmail.com>
2015-05-02 10:35:14 -0700

    simple script to build a module, will need to turn it in to a function, basically just notes at this point

---
+++ ModuleCreation.ps1
@@ -0,0 +1,26 @@
+﻿
+
+$rootPath = 'D:\ps\test'
+$moduleName = "Test"
+
+$modulePath = Join-Path $rootPath $moduleName
+$helpPath = Join-Path $modulePath en-US
+
+$HelpFile = Join-Path $helpPath "$moduleName.psm1-help.xml"
+
+mkdir $helpPath -Force #create both module folder and help folder
+
+New-ModuleManifest $modulePath -RootModule "$moduleName.psm1" -ModuleVersion 0.0.0.1
+
+
+
+New-Item -Path $modulePath -Name "${moduleName}_${guid}_HelpInfo.xml" -ItemType File -Value "" ## templated, get guid from psd1
+New-Item -Path $helpPath -Name "$moduleName.psm1-help.xml" -ItemType File -Value "" #pull value from template some place
+
+
+#inject templated files
+#look in content\module
+## assumed variables based on Replace: $moduleName, $ShortDescription
+
+iex ("@`"`n{0}`n`"@" -f (gc .\ReadMe.md -Raw)) | Out-File ReadMe.md
+cp .\module.psm1 $moduleName.psm1
