bbc3cce27db8b892aef5df569730548071eaeab2
TheDollarDes <desmond.pierre@gmail.com>
2017-04-16 06:23:15 +0100

    Powershell 5 and GitHub OAuth fixes
    You'll now require a Personal Access Token to access github, not a
    password.

--- ModuleCreation.ps1
+++ ModuleCreation.ps1
@@ -1,26 +1,27 @@
 ﻿
+# This has the potential to do damamge. Disabled.
 
-$rootPath = 'D:\ps\test'
-$moduleName = "Test"
+# $rootPath = 'D:\ps\test'
+# $moduleName = "Test"
 
-$modulePath = Join-Path $rootPath $moduleName
-$helpPath = Join-Path $modulePath en-US
+# $modulePath = Join-Path $rootPath $moduleName
+# $helpPath = Join-Path $modulePath en-US
 
-$HelpFile = Join-Path $helpPath "$moduleName.psm1-help.xml"
+# $HelpFile = Join-Path $helpPath "$moduleName.psm1-help.xml"
 
-mkdir $helpPath -Force #create both module folder and help folder
+# mkdir $helpPath -Force #create both module folder and help folder
 
-New-ModuleManifest $modulePath -RootModule "$moduleName.psm1" -ModuleVersion 0.0.0.1
-
-
-
-New-Item -Path $modulePath -Name "${moduleName}_${guid}_HelpInfo.xml" -ItemType File -Value "" ## templated, get guid from psd1
-New-Item -Path $helpPath -Name "$moduleName.psm1-help.xml" -ItemType File -Value "" #pull value from template some place
+# New-ModuleManifest $modulePath -RootModule "$moduleName.psm1" -ModuleVersion 0.0.0.1
 
 
-#inject templated files
-#look in content\module
-## assumed variables based on Replace: $moduleName, $ShortDescription
 
-iex ("@`"`n{0}`n`"@" -f (gc .\ReadMe.md -Raw)) | Out-File ReadMe.md
-cp .\module.psm1 $moduleName.psm1
+# New-Item -Path $modulePath -Name "${moduleName}_${guid}_HelpInfo.xml" -ItemType File -Value "" ## templated, get guid from psd1
+# New-Item -Path $helpPath -Name "$moduleName.psm1-help.xml" -ItemType File -Value "" #pull value from template some place
+
+
+# #inject templated files
+# #look in content\module
+# ## assumed variables based on Replace: $moduleName, $ShortDescription
+
+# iex ("@`"`n{0}`n`"@" -f (gc .\ReadMe.md -Raw)) | Out-File ReadMe.md
+# cp .\module.psm1 $moduleName.psm1
