d2049210c8e8c90655e420c11c9ca322ab748629
Joel Bennett <Jaykul@HuddledMasses.org>
2018-09-23 00:57:36 -0400

    Extract SetModuleContent to wrap reading and joining into a module

---
+++ Source/Private/SetModuleContent.ps1
@@ -0,0 +1,66 @@
+function SetModuleContent {
+    <#
+        .SYNOPSIS
+            A wrapper for Set-Content that handles arrays of file paths
+        .DESCRIPTION
+            The implementation here is strongly dependent on Build-Module doing the right thing
+            Build-Module can optionally pass a PREFIX or SUFFIX, but otherwise only passes files
+
+            Because of that, SetModuleContent doesn't test for that
+
+            The goal here is to pretend this is a pipeline, for the sake of memory and file IO
+    #>
+    [CmdletBinding()]
+    param(
+        # Where to write the joined output
+        [Parameter(Position=0, Mandatory)]
+        [string]$OutputPath,
+
+        # Input files, the scripts that will be copied to the output path
+        # The FIRST and LAST items can be text content instead of file paths.
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias("PSPath", "FullName")]
+        [string[]]$SourceFile,
+
+        # The working directory (allows relative paths for other values)
+        [string]$WorkingDirectory = $pwd,
+
+        # The encoding defaults to UTF8 (or UTF8NoBom on Core)
+        [Parameter(DontShow)]
+        [string]$Encoding = $(if($IsCoreCLR) { "UTF8NoBom" } else { "UTF8" })
+    )
+    begin {
+        Push-Location $WorkingDirectory -StackName SetModuleContent
+        $ContentStarted = $false # There has been no content yet
+
+        # Create a proxy command style scriptblock for Set-Content to keep the file handle open
+        $SetContentCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Management\Set-Content', [System.Management.Automation.CommandTypes]::Cmdlet)
+        $SetContent = {& $SetContentCmd -Path $OutputPath -Encoding $Encoding }.GetSteppablePipeline($myInvocation.CommandOrigin)
+        $SetContent.Begin($true)
+    }
+    process  {
+        foreach($file in $SourceFile) {
+            if($SourceName = Resolve-Path $file -Relative -ErrorAction SilentlyContinue) {
+                Write-Verbose "Adding $SourceName"
+                $SetContent.Process("#Region '$SourceName' 0")
+                Get-Content $SourceName -OutVariable source | ForEach-Object { $SetContent.Process($_) }
+                $SetContent.Process("#EndRegion '$SourceName' $($Source.Count)")
+            } else {
+                if(!$ContentStarted) {
+                    $SetContent.Process("#Region 'PREFIX' 0")
+                    $SetContent.Process($file)
+                    $SetContent.Process("#EndRegion 'PREFIX'")
+                    $ContentStarted = $true
+                } else {
+                    $SetContent.Process("#Region 'SUFFIX' 0")
+                    $SetContent.Process($file)
+                    $SetContent.Process("#EndRegion 'SUFFIX'")
+                }
+            }
+        }
+    }
+    end {
+        $SetContent.End()
+        Pop-Location -StackName SetModuleContent
+    }
+}
---
+++ Tests/Private/JoinScript.Tests.ps1
@@ -0,0 +1,137 @@
+Describe "JoinScript" {
+
+    Context "Necessary Parameters" {
+        $CommandInfo = InModuleScope ModuleBuilder { Get-Command JoinScript }
+
+        It "has a mandatory string OutputPath parameter" {
+            $OutputPath = $CommandInfo.Parameters['OutputPath']
+            $OutputPath | Should -Not -BeNullOrEmpty
+            $OutputPath.ParameterType | Should -Be ([String])
+            $OutputPath.Attributes.Where{$_ -is [Parameter]}.Mandatory | Should -Be $True
+        }
+
+        It "has a mandatory string array SourceFile parameter" {
+            $SourceFile = $CommandInfo.Parameters['SourceFile']
+            $SourceFile | Should -Not -BeNullOrEmpty
+            $SourceFile.ParameterType | Should -Be ([String[]])
+            $SourceFile.Attributes.Where{$_ -is [Parameter]}.Mandatory | Should -Be $True
+        }
+
+        It "has an optional string WorkingDirectory parameter" {
+            $WorkingDirectory = $CommandInfo.Parameters['WorkingDirectory']
+            $WorkingDirectory | Should -Not -BeNullOrEmpty
+            $WorkingDirectory.ParameterType | Should -Be ([String])
+            $WorkingDirectory.Attributes.Where{$_ -is [Parameter]}.Mandatory | Should -Be $False
+        }
+
+        It "has an optional string Encoding parameter" {
+            $Encoding = $CommandInfo.Parameters['Encoding']
+            $Encoding | Should -Not -BeNullOrEmpty
+            $Encoding.ParameterType | Should -Be ([String])
+            $Encoding.Attributes.Where{$_ -is [Parameter]}.Mandatory | Should -Be $False
+        }
+
+        $CommandInfo.Parameters['OutputPath']
+    }
+
+
+    Context "Joining files into one" {
+        ${global:mock get content index} = 1
+
+        Mock Get-Content -ModuleName ModuleBuilder {
+            "Script Content"
+            "File $((${global:mock get content index}++))"
+            "From $Path"
+        }
+
+        Mock Resolve-Path -ModuleName ModuleBuilder {
+            $path -replace "TestDrive:\\", ".\"
+        } -ParameterFilter { $Relative }
+
+
+
+        InModuleScope ModuleBuilder {
+            $Files = "TestDrive:\Private\First.ps1",
+                     "TestDrive:\Private\Second.ps1",
+                     "TestDrive:\Public\Third.ps1"
+            JoinScript -Source $Files -Output TestDrive:\Output.psm1
+        }
+
+        It "Calls get-content on every source file" {
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Private\First.ps1" }
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Private\Second.ps1" }
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Public\Third.ps1" }
+        }
+
+        It "Copies all three files into the Output" {
+            $Content = Get-Content TestDrive:\Output.psm1 -Raw
+            $Content | Should -Match "File 1"
+            $Content | Should -Match "First.ps1"
+
+            $Content | Should -Match "File 2"
+            $Content | Should -Match "Second.ps1"
+
+            $Content | Should -Match "File 3"
+            $Content | Should -Match "Third.ps1"
+        }
+    }
+
+    Context "Supports adding Prefix and Suffix content" {
+        ${global:mock get content index} = 1
+
+        Mock Get-Content -ModuleName ModuleBuilder {
+            "Script Content"
+            "File $((${global:mock get content index}++))"
+            "From $Path"
+        }
+
+        Mock Resolve-Path -ModuleName ModuleBuilder {
+            if ($path -match "TestDrive:") {
+                $path -replace "TestDrive:\\", ".\"
+            } else {
+                write-error "$path not found"
+            }
+        } -ParameterFilter { $Relative }
+
+
+        InModuleScope ModuleBuilder {
+            $Files = "using module Configuration",
+                     "TestDrive:\Private\First.ps1",
+                     "TestDrive:\Private\Second.ps1",
+                     "TestDrive:\Public\Third.ps1",
+                     "Export-ModuleMember Stuff"
+            JoinScript -Source $Files -Output TestDrive:\Output.psm1
+        }
+
+        It "Calls get-content on every source file" {
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Private\First.ps1" }
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Private\Second.ps1" }
+            Assert-MockCalled Get-Content -ModuleName ModuleBuilder -ParameterFilter { $Path -eq ".\Public\Third.ps1" }
+        }
+
+        It "Copies all three files into the Output" {
+            $Content = Get-Content TestDrive:\Output.psm1 -Raw
+            $Content | Should -Match "File 1"
+            $Content | Should -Match "First.ps1"
+
+            $Content | Should -Match "File 2"
+            $Content | Should -Match "Second.ps1"
+
+            $Content | Should -Match "File 3"
+            $Content | Should -Match "Third.ps1"
+        }
+
+        It "Includes the prefix" {
+            $Content = Get-Content TestDrive:\Output.psm1 -Raw
+            $Content | Should -Match "#Region 'PREFIX' 0"
+            $Content | Should -Match "using module Configuration"
+        }
+
+        It "Includes the suffix" {
+            $Content = Get-Content TestDrive:\Output.psm1 -Raw
+            $Content | Should -Match "#Region 'SUFFIX' 0"
+            $Content | Should -Match "Export-ModuleMember Stuff"
+        }
+    }
+
+}
