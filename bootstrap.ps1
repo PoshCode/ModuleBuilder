87d938e24a8e4737d0267f367fa377db1aa4c2b6
Joel Bennett <Jaykul@HuddledMasses.org>
2018-10-02 01:15:26 -0400

    clean up bootstrap code so it's only in one place

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,22 +1,43 @@
 [CmdletBinding()]
 param(
-    [ValidateSet("CurrentUser", "AllUsers")]
-    $Scope="CurrentUser"
+    #
+    [ValidateSet("CurrentUser", "AllUsers", "LocalTools")]
+    $Scope = "CurrentUser"
 )
 
-Push-Location $PSScriptRoot
+Push-Location $PSScriptRoot -StackName bootstrap-stack
 
-# # This really does work, but using PSDepends is a lot smoother when there's more modules, or nuget packages
+# # This would probably work, if the default gallery was trusted ...
 # Install-Module Pester        -RequiredVersion 4.4.0 -Scope CurrentUser -Repository PSGallery -SkipPublisherCheck
 # Install-Module Configuration -RequiredVersion 1.3.1 -Scope CurrentUser -Repository PSGallery -SkipPublisherCheck
+if (!(Get-Module PSDepends -ListAvailable)) {
+    $Policy = (Get-PSRepository PSGallery).InstallationPolicy
+    try {
+        Set-PSRepository PSGallery -InstallationPolicy Trusted
 
-# Here's one example of why PowerShellGet is so awkward: their default gallery is not be trusted by default!
-$Policy = (Get-PSRepository PSGallery).InstallationPolicy
-Set-PSRepository PSGallery -InstallationPolicy Trusted
-try {
-    Install-Module PSDepend -RequiredVersion 0.3.0 -Scope:$Scope -Repository PSGallery -ErrorAction Stop
-} finally {
-    # Make sure we didn't change anything permanently
-    Set-PSRepository PSGallery -InstallationPolicy:$Policy
+        if ($Scope -eq "LocalTools") {
+            $Target = Join-Path $PSScriptRoot "Tools"
+            New-Item -Path $Target -ItemType Directory -Force
+
+            $Env:PSModulePath += ';' + $Target
+            $PSDefaultParameterValues["Invoke-PSDepend:Target"] = $Target
+            Save-Module PSDepend -Repository PSGallery -ErrorAction Stop -Path $Target
+        } else {
+            $PSDefaultParameterValues["Invoke-PSDepend:Target"] = $Scope
+            Install-Module PSDepend -Repository PSGallery -ErrorAction Stop -Scope:$Scope
+        }
+    } finally {
+        # Make sure we didn't change anything permanently
+        Set-PSRepository PSGallery -InstallationPolicy:$Policy
+        Pop-Location -StackName bootstrap-stack
+    }
 }
-Import-Module  PSDepend -RequiredVersion 0.3.0
+
+try {
+    Write-Verbose "Updating dependencies"
+    Import-Module PSDepend -ErrorAction Stop
+    Invoke-PSDepend -Import -Force -ErrorAction Stop
+} catch {
+    Write-Warning "Unable to restore dependencies. Please review errors:"
+    throw
+}
