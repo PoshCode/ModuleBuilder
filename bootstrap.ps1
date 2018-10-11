b91f2654dd32ca46835c5c082c022f387634e69a
Joel Bennett <Jaykul@HuddledMasses.org>
2018-10-10 22:46:35 -0400

    Put back PSGallery InstallationPolicy after bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -8,6 +8,9 @@ param(
 [ModuleSpecification[]]$RequiredModules = Import-LocalizedData -BaseDirectory $PSScriptRoot -FileName RequiredModules
 $Policy = (Get-PSRepository PSGallery).InstallationPolicy
 Set-PSRepository PSGallery -InstallationPolicy Trusted
-
-$RequiredModules | Install-Module -Scope $Scope -Repository PSGallery -SkipPublisherCheck -Verbose
+try {
+    $RequiredModules | Install-Module -Scope $Scope -Repository PSGallery -SkipPublisherCheck -Verbose
+} finally {
+    Set-PSRepository PSGallery -InstallationPolicy $Policy
+}
 $RequiredModules | Import-Module
