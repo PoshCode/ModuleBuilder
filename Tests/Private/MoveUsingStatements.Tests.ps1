0a23b841d2fd5e30616408d9425f149bcdfc1784
gaelcolas <gaelcolas@synedgy.com>
2018-11-23 14:23:45 +0800

    adding function to Move Using statements to the Top using AST.

---
+++ Tests/Private/MoveUsingStatements.Tests.ps1
@@ -0,0 +1,7 @@
+Describe "MoveUsingStatements" {
+    # Import-Module ModuleBuilder -DisableNameChecking -Verbose:$False
+
+    # It 'Should get the SourceFile and LineNumber from stack trace messages with modules' {
+    #     $Source = InModuleScope ModuleBuilder { }
+    # }
+}
