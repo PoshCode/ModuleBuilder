44ab1c6204d5155a0a63b27a7e100d4c29296d66
gaelcolas <gaelcolas@synedgy.com>
2019-01-18 13:18:18 +0800

    adding the PowerShellGet version in verbose output for troubleshooting

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -8,6 +8,7 @@ param(
 [ModuleSpecification[]]$RequiredModules = Import-LocalizedData -BaseDirectory $PSScriptRoot -FileName RequiredModules
 $Policy = (Get-PSRepository PSGallery).InstallationPolicy
 Set-PSRepository PSGallery -InstallationPolicy Trusted
+Write-Verbose "Using PowerShellGet Version $((Get-Module PowerShellGet | Select-Object -First 1).Version)"
 try {
     $RequiredModules | Install-Module -Scope $Scope -Repository PSGallery -SkipPublisherCheck -Verbose
 } finally {
