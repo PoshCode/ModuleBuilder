f66739ede619819758d0e9d7aa31ae68f31a050b
Joel Bennett <Jaykul@HuddledMasses.org>
2019-03-07 01:22:40 -0500

    A better fix for bootstrapping
    The earlier change doesn't help with build servers if they only have 1.0

--- RequiredModules.psd1
+++ RequiredModules.psd1
@@ -1,4 +1,6 @@
 @(
-    @{ ModuleName = "Configuration"; RequiredVersion = "1.3.1"}
-    @{ ModuleName = "Pester"; RequiredVersion = "4.4.0"}
+    # In RequiredModules, always use REQUIREDVERSION
+    @{ ModuleName = "PowerShellGet"; RequiredVersion = "2.0.4" }
+    @{ ModuleName = "Configuration"; RequiredVersion = "1.3.1" }
+    @{ ModuleName = "Pester";        RequiredVersion = "4.4.2" }
 )
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,18 +1,40 @@
-#requires -Modules @{ModuleName='PowerShellGet'; ModuleVersion='2.0.0'}
-
 using namespace Microsoft.PowerShell.Commands
-[CmdletBinding()]
+<#
+.SYNOPSIS
+    Installs and imports modules listed in RequiredModules if they're missing
+.EXAMPLE
+    .\bootstrap
+
+    Run the bootstrap interactively, with a prompt for each module to be installed
+.EXAMPLE
+    .\bootstrap -Confirm:$false
+
+    Run the bootstrap and install all the modules without prompting
+#>
+[CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact="High")]
 param(
     # Override the default scope if you wish -- now that PowerShellGet has sane defaults, we use their default
     [ValidateSet("CurrentUser", "AllUsers")]
-    $Scope
+    $Scope = "CurrentUser"
 )
 [ModuleSpecification[]]$RequiredModules = Import-LocalizedData -BaseDirectory $PSScriptRoot -FileName RequiredModules
+
+# Force Policy to Trusted so we can install without prompts and without -Force which is bad
 $Policy = (Get-PSRepository PSGallery).InstallationPolicy
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 try {
-    $RequiredModules | Install-Module @PSBoundParameters -Repository PSGallery -SkipPublisherCheck -Verbose
+    # Check for the modules by hand so that "RequiredVersion" is treated as "MinimumVersion"
+    $RequiredModules.Where{
+        -not (Get-Module $_.Name -ListAvailable | Where-Object Version -ge $_.RequiredVersion)
+    } |
+    # Install missing modules with -AlloClobber and -SkipPublisherCheck because PowerShellGet requires both
+    Install-Module -Scope $Scope -Repository PSGallery -SkipPublisherCheck -AllowClobber -Verbose -Confirm:$($ConfirmPreference -ne "None")
+# Put Policy back so we don't needlessly change environments permanently
 } finally {
     Set-PSRepository PSGallery -InstallationPolicy $Policy
 }
-$RequiredModules | Import-Module
+
+# Since we're now allowing newer versions, import the newest available
+$RequiredModules.ForEach{
+    Get-Module $_.Name -ListAvailable | Sort-Object Version | Select-Object -Last 1
+} | Import-Module
