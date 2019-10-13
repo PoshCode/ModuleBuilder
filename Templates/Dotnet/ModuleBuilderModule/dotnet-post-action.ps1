2f91df3410cfe55224fabd37b24fa19c9bfbf35d
bravo-kernel <bravo-kernel@users.noreply.github.com>
2019-10-13 13:43:14 +0200

    fix: make sure generated manifest is UTF-8 without BOM

--- Templates/Dotnet/ModuleBuilderModule/dotnet-post-action.ps1
+++ Templates/Dotnet/ModuleBuilderModule/dotnet-post-action.ps1
@@ -4,8 +4,14 @@
 #>
 Set-StrictMode -Version Latest
 
-# Generate manifest
-New-ModuleManifest -RootModule "{moduleName}" -Path "./Source/{moduleName}.psd1" -ModuleVersion 0.0.1
+# generate manifest
+$manifestPath = "./Source/{moduleName}.psd1"
+New-ModuleManifest -RootModule "{moduleName}" -Path $manifestPath -ModuleVersion 0.0.1
 
-# Remove self
+# convert manifest to UTF-8 without BOM
+$content = Get-Content $manifestPath
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+[System.IO.File]::WriteAllLines($manifestPath, $content, $Utf8NoBomEncoding)
+
+# delete self
 Remove-Item ./dotnet-post-action.ps1
