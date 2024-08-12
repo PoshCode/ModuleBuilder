8a5f6f1cf745ddb73e0e7de4bde8972c884e93ec
Joel Bennett <Jaykul@HuddledMasses.org>
2024-08-12 00:59:36 -0400

    Add Invoke-ScriptGenerator
    semver:feature

--- Source/Private/ConvertToAst.ps1
+++ Source/Private/ConvertToAst.ps1
@@ -13,18 +13,26 @@ function ConvertToAst {
         Write-Debug "    ENTER: ConvertToAst $Code"
         $ParseErrors = $null
         $Tokens = $null
-        if ($Code | Test-Path -ErrorAction SilentlyContinue) {
-            Write-Debug "      Parse Code as Path"
-            $AST = [System.Management.Automation.Language.Parser]::ParseFile(($Code | Convert-Path), [ref]$Tokens, [ref]$ParseErrors)
-        } elseif ($Code -is [System.Management.Automation.FunctionInfo]) {
-            Write-Debug "      Parse Code as Function"
-            $String = "function $($Code.Name) { $($Code.Definition) }"
-            $AST = [System.Management.Automation.Language.Parser]::ParseInput($String, [ref]$Tokens, [ref]$ParseErrors)
-        } else {
-            Write-Debug "      Parse Code as String"
-            $AST = [System.Management.Automation.Language.Parser]::ParseInput([String]$Code, [ref]$Tokens, [ref]$ParseErrors)
-        }
 
+        if ($Code -is [System.Management.Automation.FunctionInfo]) {
+            Write-Debug "      Parse Code as Function"
+            $AST = [System.Management.Automation.Language.Parser]::ParseInput($Code.Definition, "function:$($Code.Name)", [ref]$Tokens, [ref]$ParseErrors)
+        } else {
+            $Provider = $null
+            try {
+                [string[]]$Files = $ExecutionContext.SessionState.Path.GetResolvedProviderPathFromPSPath($Code, [ref]$Provider)
+            } catch {
+                Write-Debug ("Exception resolving Code as Path " + $_.Exception.Message)
+            }
+
+            if ($Provider.Name -eq "FileSystem" -and $Files.Count -gt 0) {
+                Write-Debug "      Parse Code as File Path"
+                $AST = [System.Management.Automation.Language.Parser]::ParseFile(($Files[0] | Convert-Path), [ref]$Tokens, [ref]$ParseErrors)
+            } else {
+                Write-Debug "      Parse Code as String"
+                $AST = [System.Management.Automation.Language.Parser]::ParseInput([String]$Code, [ref]$Tokens, [ref]$ParseErrors)
+            }
+        }
         Write-Debug "    EXIT: ConvertToAst"
         [PSCustomObject]@{
             PSTypeName  = "PoshCode.ModuleBuilder.ParseResults"
