8c1bf83f0be5a07f580cda0774cbcf8b661d6860
Joel Bennett <Jaykul@HuddledMasses.org>
2024-08-21 22:47:26 -0400

    Give in and add a second parameter set on ConvertToAst so it can properly handle paths vs code strings.
    This is a breaking change on this private function.

--- Source/Private/ConvertToAst.ps1
+++ Source/Private/ConvertToAst.ps1
@@ -3,42 +3,66 @@ function ConvertToAst {
         .SYNOPSIS
             Parses the given code and returns an object with the AST, Tokens and ParseErrors
     #>
+    [CmdletBinding(DefaultParameterSetName="Path")]
     param(
         # The script content, or script or module file path to parse
-        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        [Alias("Path", "PSPath", "Definition", "ScriptBlock", "Module")]
-        $Code
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "Code")]
+        [Alias("ScriptBlock")]
+        $Code,
+
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = "Command", Position = 0)]
+        [System.Management.Automation.FunctionInfo]$Command,
+
+        [Parameter(ValueFromPipelineByPropertyName, Position = 1)]
+        [Alias("PSPath", "File", "Definition")]
+        [string]$Path
     )
     process {
-        Write-Debug "    ENTER: ConvertToAst $(($Code -split "[\r\n]")[0])"
+        Write-Debug "    ENTER: ConvertToAst $Path $(("$Command$Code" -split "[\r\n]")[0])"
         $ParseErrors = $null
         $Tokens = $null
 
-        if ($Code -is [System.Management.Automation.FunctionInfo]) {
-            Write-Debug "      Parse Code as Function"
-            $AST = [System.Management.Automation.Language.Parser]::ParseInput($Code.Definition, "function:$($Code.Name)", [ref]$Tokens, [ref]$ParseErrors)
-        } else {
-            $Provider = $null
-            if (@($Code).Count -eq 1 -and $Code -notmatch "\n") {
+        switch($PSCmdlet.ParameterSetName) {
+            "Path" {
+                $Provider = $null
                 try {
-                    [string[]]$Files = $ExecutionContext.SessionState.Path.GetResolvedProviderPathFromPSPath($Code, [ref]$Provider)
+                    $Path = $ExecutionContext.SessionState.Path.GetResolvedProviderPathFromPSPath($Path, [ref]$Provider)[0]
                 } catch {
-                    Write-Debug ("Exception resolving Code as Path " + $_.Exception.Message)
+                    Write-Debug ("      Exception resolving $Path as Path " + $_.Exception.Message)
+                }
+                if ($Provider.Name -eq "FileSystem") {
+                    Write-Debug "      Parse File Path: $Path"
+                    $AST = [System.Management.Automation.Language.Parser]::ParseFile($Path, [ref]$Tokens, [ref]$ParseErrors)
+                } else {
+                    Write-Debug "      Parse Code $(($Path -split "[\r\n]")[0])"
+                    $AST = [System.Management.Automation.Language.Parser]::ParseInput($Path, [ref]$Tokens, [ref]$ParseErrors)
                 }
             }
-            if ($Provider.Name -eq "FileSystem" -and $Files.Count -gt 0) {
-                Write-Debug "      Parse Code as File Path"
-                $AST = [System.Management.Automation.Language.Parser]::ParseFile(($Files[0] | Convert-Path), [ref]$Tokens, [ref]$ParseErrors)
-            } else {
-                Write-Debug "      Parse Code as String"
-                $AST = [System.Management.Automation.Language.Parser]::ParseInput([String]$Code, [ref]$Tokens, [ref]$ParseErrors)
+            "Command" {
+                Write-Debug "      Parse Function"
+                if (!$Path) {
+                    $Path = "function:$($Command.Name)"
+                }
+                $AST = [System.Management.Automation.Language.Parser]::ParseInput($Command.Definition, $Path, [ref]$Tokens, [ref]$ParseErrors)
+            }
+            "Code" {
+                Write-Debug "      Parse Code as ScriptBlock"
+                if ($Code -is [System.Management.Automation.ScriptBlock]) {
+                    $Code = $Code.GetNewClosure().Invoke().ToString()
+                }
+                if (!$Path) {
+                    $Path = "scriptblock"
+                }
+                $AST = [System.Management.Automation.Language.Parser]::ParseInput($Code, $Path, [ref]$Tokens, [ref]$ParseErrors)
             }
         }
-        Write-Debug "    EXIT: ConvertToAst"
+
+        Write-Debug "    EXIT: ConvertToAst $Path"
         [PSCustomObject]@{
             PSTypeName  = "PoshCode.ModuleBuilder.ParseResults"
             ParseErrors = $ParseErrors
             Tokens      = $Tokens
+            Path        = $Path
             AST         = $AST
         }
     }
