0bd293bad947e1e2d9ed7e2a0e6d0a239dae49b2
Joel Bennett <Jaykul@HuddledMasses.org>
2025-02-07 00:29:00 -0500

    Fix the last set of tests broken by Pester 5 scope

--- Tests/Integration/Parameters.Tests.ps1
+++ Tests/Integration/Parameters.Tests.ps1
@@ -4,28 +4,25 @@ Describe "Parameters.Set in build manifest" -Tag Integration {
     BeforeAll {
         . $PSScriptRoot/../Convert-FolderSeparator.ps1
         New-Item $PSScriptRoot/Result3/Parameters/ReadMe.md -ItemType File -Force
-
-        $Output = Build-Module $PSScriptRoot/Parameters/build.psd1
-        if ($Output) {
-            $Module = [IO.Path]::ChangeExtension($Output.Path, "psm1")
-            $Metadata = Import-Metadata $Output.Path
-        }
-
     }
 
-    It "Passthru works" {
+    It "Passthru is read from the build manifest" {
+        $Output = Build-Module "$PSScriptRoot/Parameters/build.psd1"
         $Output | Should -Not -BeNullOrEmpty
+        $Output.Path | Convert-FolderSeparator | Should -Be (Convert-FolderSeparator "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psd1")
     }
 
     It "The Target is Build" {
-        "$PSScriptRoot/Result3/Parameters/ReadMe.md" | Should -Exist
+        "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psm1" | Should -Exist
     }
 
     It "The version is set" {
+        $Metadata = Import-Metadata "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psd1"
         $Metadata.ModuleVersion | Should -Be "3.0.0"
     }
 
     It "The PreRelease is set" {
+        $Metadata = Import-Metadata "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psd1"
         $Metadata.PrivateData.PSData.Prerelease | Should -Be 'alpha001'
     }
 }
