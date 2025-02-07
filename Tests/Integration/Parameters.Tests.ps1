f6af40e96465230f61f8e6c7ec2dddc0632aa2b6
Joel Bennett <Jaykul@HuddledMasses.org>
2025-02-07 00:45:55 -0500

    Add verbose to this test that won't pass

--- Tests/Integration/Parameters.Tests.ps1
+++ Tests/Integration/Parameters.Tests.ps1
@@ -7,13 +7,13 @@ Describe "Parameters.Set in build manifest" -Tag Integration {
     }
 
     It "Passthru is read from the build manifest" {
-        $Output = Build-Module "$PSScriptRoot/Parameters/build.psd1"
+        $Output = Build-Module (Convert-FolderSeparator "$PSScriptRoot/Parameters/build.psd1") -Verbose
         $Output | Should -Not -BeNullOrEmpty
         $Output.Path | Convert-FolderSeparator | Should -Be (Convert-FolderSeparator "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psd1")
     }
 
     It "The Target is Build" {
-        "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psm1" | Should -Exist
+        Convert-FolderSeparator "$PSScriptRoot/Result3/Parameters/3.0.0/Parameters.psm1" | Should -Exist
     }
 
     It "The version is set" {
