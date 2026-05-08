40e0a7996c98902dd7b417466a63e88a5f4a5a97
Joel Bennett <Jaykul@HuddledMasses.org>
2026-05-07 22:34:47 -0400

    Fix some test discovery scope issues

--- Tests/Public/ConvertFrom-SourceLineNumber.Tests.ps1
+++ Tests/Public/ConvertFrom-SourceLineNumber.Tests.ps1
@@ -1,19 +1,36 @@
 Describe "ConvertFrom-SourceLineNumber" {
     # use the integration test code
+    BeforeDiscovery {
+        ${\} = [io.path]::DirectorySeparatorChar
+
+        $TestCases = @(
+            @{
+                outputLine = 40;
+                sourceFile = ".${\}Private${\}TestUnExportedAliases.ps1";
+                sourceLine = 13;
+                Module     = "$PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1"
+            }
+            @{
+                outputLine = 48;
+                sourceFile = ".${\}Public${\}Get-Source.ps1";
+                sourceLine = 5;
+                Module     = "$PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1"
+            }
+            @{
+                outputLine = 56;
+                sourceFile = ".${\}Public${\}Set-Source.ps1";
+                sourceLine = 3;
+                Module     = "$PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1"
+            }
+        )
+    }
     BeforeAll {
-        Build-Module $PSScriptRoot/../Integration/Source1/build.psd1 -Passthru
+        ${\} = [io.path]::DirectorySeparatorChar
+        Build-Module "$PSScriptRoot${\}..${\}Integration${\}Source1${\}build.psd1" -Passthru
         Push-Location $PSScriptRoot -StackName ConvertFrom-SourceLineNumber
 
-        $global:Convert_LineNumber_ModulePath = Convert-Path "$PSScriptRoot/../Integration/Result1/Source1/1.0.0/Source1.psm1"
-        $global:Convert_LineNumber_ModuleSource = Convert-Path "$PSScriptRoot/../Integration/Source1"
-        $global:Convert_LineNumber_ModuleContent = Get-Content $global:Convert_LineNumber_ModulePath
-        ${global:\} = [io.path]::DirectorySeparatorChar
-
-        $global:TestCases = @(
-            @{ outputLine = 40; sourceFile = ".${\}Private${\}TestUnExportedAliases.ps1"; sourceLine = 13; Module = $Convert_LineNumber_ModulePath }
-            @{ outputLine = 48; sourceFile = ".${\}Public${\}Get-Source.ps1"; sourceLine = 5; Module = $Convert_LineNumber_ModulePath }
-            @{ outputLine = 56; sourceFile = ".${\}Public${\}Set-Source.ps1"; sourceLine = 3; Module = $Convert_LineNumber_ModulePath }
-        )
+        $Convert_LineNumber_ModuleContent = Get-Content "$PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1"
+        ${\} = [io.path]::DirectorySeparatorChar
     }
     AfterAll {
         Pop-Location -StackName ConvertFrom-SourceLineNumber
@@ -22,9 +39,10 @@ Describe "ConvertFrom-SourceLineNumber" {
     It "Should map line <sourceLine> in the source of <sourceFile> to line <outputLine> in the Module" -TestCases $TestCases {
         param($outputLine, $sourceFile, $sourceLine, $module)
 
-        $sourcePath = Join-Path $Convert_LineNumber_ModuleSource $SourceFile | Convert-Path
+        ${\} = [io.path]::DirectorySeparatorChar
+        $sourcePath = Join-Path "$PSScriptRoot${\}..${\}Integration${\}Source1" $SourceFile | Convert-Path
         $OutputLocation = ConvertFrom-SourceLineNumber $sourcePath $sourceLine -Module $module
-        # $OutputLocation.Script | Should -Be $Convert_LineNumber_ModulePath
+        # $OutputLocation.Script | Should -Be "$PSScriptRoot/../Integration/Result1/Source1/1.0.0/Source1.psm1"
         $OutputLocation.Line | Should -Be $outputLine
 
         $line = (Get-Content $sourcePath)[$sourceLine - 1]
@@ -36,7 +54,8 @@ Describe "ConvertFrom-SourceLineNumber" {
     }
 
     It "Should warn if the SourceFile doesn't exist" {
-        ConvertFrom-SourceLineNumber -SourceFile TestDrive:/NoSuchFile -SourceLineNumber 10 -Module $Convert_LineNumber_ModulePath -WarningVariable Warns
-        $Warns | Should -Be "'TestDrive:/NoSuchFile' not found in $Convert_LineNumber_ModulePath"
+        ${\} = [io.path]::DirectorySeparatorChar
+        ConvertFrom-SourceLineNumber -SourceFile "TestDrive:${\}NoSuchFile" -SourceLineNumber 10 -Module "$PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1" -WarningVariable Warns
+        $Warns | Should -Be "'TestDrive:${\}NoSuchFile' not found in $PSScriptRoot${\}..${\}Integration${\}Result1${\}Source1${\}1.0.0${\}Source1.psm1"
     }
 }
