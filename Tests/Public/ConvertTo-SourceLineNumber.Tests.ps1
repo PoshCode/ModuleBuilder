52d225c76f7f6b5781fb851f816f40f9b5166bff
Joel Bennett <Jaykul@HuddledMasses.org>
2026-05-19 22:52:02 -0400

    Refactor test that's failing in the matrix

--- Tests/Public/ConvertTo-SourceLineNumber.Tests.ps1
+++ Tests/Public/ConvertTo-SourceLineNumber.Tests.ps1
@@ -1,4 +1,12 @@
 Describe "ConvertTo-SourceLineNumber" {
+    BeforeDiscovery {
+        ${global:\} = [io.path]::DirectorySeparatorChar
+        $TestCases = @(
+            @{ outputLine = 40; sourceFile = ".${\}Private${\}TestUnExportedAliases.ps1"; sourceLine = 13 }
+            @{ outputLine = 48; sourceFile = ".${\}Public${\}Get-Source.ps1"; sourceLine = 5 }
+            @{ outputLine = 56; sourceFile = ".${\}Public${\}Set-Source.ps1"; sourceLine = 3 }
+        )
+    }
     # use the integration test code
     BeforeAll {
         Build-Module $PSScriptRoot/../Integration/Source1/build.psd1 -Passthru
@@ -7,13 +15,6 @@ Describe "ConvertTo-SourceLineNumber" {
         $global:Convert_LineNumber_ModulePath = Convert-Path "./../Integration/Result1/Source1/1.0.0/Source1.psm1"
         $global:Convert_LineNumber_ModuleSource = Convert-Path "./../Integration/Source1"
         $global:Convert_LineNumber_ModuleContent = Get-Content $global:Convert_LineNumber_ModulePath
-        ${global:\} = [io.path]::DirectorySeparatorChar
-
-        $global:TestCases = @(
-            @{ outputLine = 40; sourceFile = ".${\}Private${\}TestUnExportedAliases.ps1"; sourceLine = 13 }
-            @{ outputLine = 48; sourceFile = ".${\}Public${\}Get-Source.ps1"; sourceLine = 5 }
-            @{ outputLine = 56; sourceFile = ".${\}Public${\}Set-Source.ps1"; sourceLine = 3 }
-        )
     }
     AfterAll {
         Pop-Location -StackName ConvertTo-SourceLineNumber
@@ -28,7 +29,7 @@ Describe "ConvertTo-SourceLineNumber" {
 
         $line = (Get-Content (Join-Path $Convert_LineNumber_ModuleSource $SourceLocation.SourceFile))[$SourceLocation.SourceLineNumber - 1]
         try {
-            $Convert_LineNumber_ModuleContent[$outputLine -1] | Should -Be $line
+            $Convert_LineNumber_ModuleContent[$outputLine - 1] | Should -Be $line
         } catch {
             throw "Failed to match module line $outputLine to $($SourceLocation.SourceFile) line $($SourceLocation.SourceLineNumber).`nExpected $Line`nBut got  $($Convert_LineNumber_ModuleContent[$outputLine -1])"
         }
@@ -40,7 +41,7 @@ Describe "ConvertTo-SourceLineNumber" {
     }
 
     It 'Should work with an error PositionMessage' {
-        $line = Select-String -Path $Convert_LineNumber_ModulePath 'function Set-Source {' | ForEach-Object LineNumber
+        $line = (Select-String -Path $Convert_LineNumber_ModulePath 'function Set-Source {').LineNumber
 
         $SourceLocation = "At ${Convert_LineNumber_ModulePath}:$line char:17" | ConvertTo-SourceLineNumber
         $SourceLocation.SourceFile | Should -Be ".${\}Public${\}Set-Source.ps1"
@@ -51,8 +52,11 @@ Describe "ConvertTo-SourceLineNumber" {
 
         $SourceFile = Join-Path $Convert_LineNumber_ModuleSource (Join-Path Public Set-Source.ps1) | Convert-Path
 
-        $outputLine = Select-String -Path $Convert_LineNumber_ModulePath "sto͞o′pĭd" | ForEach-Object LineNumber
-        $sourceLine = Select-String -Path $SourceFile "sto͞o′pĭd" | ForEach-Object LineNumber
+        $outputLine = (Select-String -Path $Convert_LineNumber_ModulePath "sto͞o′pĭd").LineNumber
+        $sourceLine = (Select-String -Path $SourceFile "sto͞o′pĭd").LineNumber
+
+        $sourceLine | Should -BeGreaterThan 0 -Because "the test string 'sto͞o′pĭd' is definitely found in the source file"
+        $outputLine | Should -BeGreaterThan 0 -Because "the test string 'sto͞o′pĭd' should be found in the module"
 
         $SourceLocation = "At Set-Source, ${Convert_LineNumber_ModulePath}: line $outputLine" | ConvertTo-SourceLineNumber
         $SourceLocation.SourceFile | Should -Be ".${\}Public${\}Set-Source.ps1"
